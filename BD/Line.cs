947d9ac049e78b46a7bc3ade7d3192bf39053b90
yama <yama@oyama.moe>
2022-06-07 14:36:03 +0200

    Constructeur de la classe Line

--- BD/Line.cs
+++ BD/Line.cs
@@ -13,5 +13,13 @@ namespace BD
         public int station1 { get; private set; }
         public int station2 { get; private set; }
 
+
+        public Line(int lineId, string lineName, int station1, int station2)
+        {
+            this.lineId = lineId;
+            this.lineName = lineName;
+            this.station1 = station1;
+            this.station2 = station2;
+        }
     }
 }
